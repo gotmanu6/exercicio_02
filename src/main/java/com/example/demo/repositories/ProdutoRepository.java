c390fe1d2048ca9315f1a629df029c3be00064d7
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-22 21:24:23 -0300

    repository

--- src/main/java/com/example/demo/repositories/ProdutoRepository.java
+++ src/main/java/com/example/demo/repositories/ProdutoRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.repositories;
 
-import com.example.demo.models.ProdutoModel;
-import models.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<models.ProdutoModel, Long> {
